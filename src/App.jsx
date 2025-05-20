635ff4efe6cf424e41c709c18fe13abf818c49a8
Bragosi <oladejoboluwatife20003@gmail.com>
2025-05-20 21:07:28 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import ButtonGradient from "./assets/Svg/ButtonGradient"
+export default function App() {
+  return (
+    <>
+    <div className='pt-[4.75] lg:pt-[5.25] overflow-hidden'>
+        <Header/>
+    </div>
+    <ButtonGradient/>
+
+    </>
+  )
+}
---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#home",
+  },
+  {
+    id: "1",
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: "2",
+    title: "Services",
+    url: "#services",
+  },
+  {
+    id: "3",
+    title: "Contact",
+    url: "#contact",
+  },
+  {
+     id: "4",
+    title: "View Github",
+    url: "https://github.com/Bragosi",
+    onlyMobile:true
+  }
+];
